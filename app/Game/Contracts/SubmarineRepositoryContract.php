f318f0f122b4633b5fef569eb6a198516d93341f
sytzez <sytzez@gmail.com>
2021-09-12 23:27:54 +0200

    [Update] Add getAll method to submarineRepository

--- app/Game/Contracts/SubmarineRepositoryContract.php
+++ app/Game/Contracts/SubmarineRepositoryContract.php
@@ -8,5 +8,11 @@ interface SubmarineRepositoryContract
 {
     public function save(SubmarineContract $submarine): static;
 
+    /**
+     * @param GameContract $game
+     * @return iterable<SubmarineContract>
+     */
+    public function getAll(GameContract $game): iterable;
+
     public function getAtPosition(GameContract $game, Position $position): ?SubmarineContract;
 }
