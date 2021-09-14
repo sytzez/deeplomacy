d6cc5ff15b76bc6d787b9eab658e74ed6560e818
sytzez <sytzez@gmail.com>
2021-09-14 15:53:10 +0200

    [Update] Use placement action in submarine factory

--- app/Factories/SubmarineFactory.php
+++ app/Factories/SubmarineFactory.php
@@ -2,18 +2,34 @@
 
 namespace App\Factories;
 
+use App\Adapters\SubmarineAdapter;
+use App\Game\Actions\PlaceSubmarineAction;
+use App\Game\Data\PlaceSubmarineData;
+use App\Game\Strategies\RandomPlacementStrategy;
 use App\Models\Game;
 use App\Models\Submarine;
 use App\Models\User;
 
 class SubmarineFactory
 {
+    public function __construct(
+        protected PlaceSubmarineAction $placeSubmarineAction,
+    ) {
+    }
+
     public function make(User $user, Game $game): Submarine
     {
         $submarine = new Submarine();
 
         $submarine->user()->associate($user);
         $submarine->game()->associate($game);
+
+        $this->placeSubmarineAction->do(
+            new PlaceSubmarineData(
+                new SubmarineAdapter($submarine),
+                new RandomPlacementStrategy(),
+            )
+        );
 
         return $submarine;
     }
