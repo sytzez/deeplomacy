5d383639699753c5fb930d2f2c22de7b9f54539b
sytzez <sytzez@gmail.com>
2021-09-14 11:29:44 +0200

    [Setup] Setup game controller and routes

---
+++ app/Http/Controllers/GameController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class GameController extends Controller
+{
+    public function index(): Response
+    {
+        //
+    }
+
+    public function create(): Response
+    {
+        //
+    }
+
+    public function store(Request $request): Response
+    {
+        //
+    }
+
+    public function show(int $id): Response
+    {
+        //
+    }
+
+    public function edit(int $id): Response
+    {
+        //
+    }
+
+    public function update(Request $request, int $id): Response
+    {
+        //
+    }
+
+    public function destroy(int $id): Response
+    {
+        //
+    }
+}
