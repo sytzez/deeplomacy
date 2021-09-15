e545db7342f30c6b51c4c0aba5984f8672753e93
sytzez <sytzez@gmail.com>
2021-09-15 12:48:41 +0200

    [Clean] Remove comment block

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,6 @@ use App\Http\Controllers\GameController;
 use App\Http\Controllers\PlayController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::redirect('/', '/games');
 
 Route::middleware('auth.auto')->group(function () {
