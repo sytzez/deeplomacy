a8b538280574ad19c38008ddee8d4ffdd61399ad
sytzez <sytzez@gmail.com>
2021-09-15 12:37:50 +0200

    [Refactor] Refactor routing, add some routes

--- routes/web.php
+++ routes/web.php
@@ -15,24 +15,56 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/games');
 
 Route::middleware('auth.auto')->group(function () {
 
-    Route::name('games.join')
-        ->get('games/{game}/join', [GameController::class, 'join']);
+    Route::name('games.')
+        ->prefix('/games/{game}')
+        ->group(function (): void {
 
-    Route::name('games.leave')
-        ->get('games/{game}/leave', [GameController::class, 'leave']);
+            Route::name('join')
+                ->get(
+                    '/join',
+                    [GameController::class, 'join']
+                );
+
+            Route::name('leave')
+                ->get(
+                    '/leave',
+                    [GameController::class, 'leave']
+                );
+        });
 
     Route::resource('games', GameController::class)
         ->only(['index', 'create', 'store', 'show']);
 
-    Route::name('play.show')
-        ->get('play/{game}', [PlayController::class, 'show']);
+    Route::name('play.')
+        ->prefix('/play/{game}')
+        ->group(function (): void {
 
-    Route::name('play.move')
-        ->post('play/{game}/move', [PlayController::class, 'move']);
+            Route::name('show')
+                ->get(
+                    '/',
+                    [PlayController::class, 'show']
+                );
+
+            Route::name('move')
+                ->post(
+                    '/move',
+                    [PlayController::class, 'move']
+                );
+
+            Route::name('give-action-points')
+                ->post(
+                    '/give-action-points',
+                    [PlayController::class, 'giveActionPoints']
+                );
+
+            Route::name('share-sonar')
+                ->post(
+                    '/share-sonar',
+                    [PlayController::class, 'shareSonar']
+                );
+        });
 });
