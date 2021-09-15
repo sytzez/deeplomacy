b15820e63f766e5d774f54e6043bf456b79c665e
sytzez <sytzez@gmail.com>
2021-09-15 21:34:48 +0200

    [Update] Add form to give action points

--- resources/views/play/cells/submarine.blade.php
+++ resources/views/play/cells/submarine.blade.php
@@ -17,4 +17,14 @@
         <input type="submit" value="Share sonar">
 
     </form>
+
+    <form method="post" action="{{ route('play.give-action-points', [$game]) }}">
+        @csrf
+
+        <input type="hidden" name="submarine" value="{{ $cell->getSubmarine()->getModel()->id }}">
+        <label for="amount">AP:</label>
+        <input type="number" name="amount" id="amount" min="1" max="{{ $mySubmarine->getActionPoints()->getAmount() }}">
+        <input type="submit" value="Give AP">
+
+    </form>
 </div>
