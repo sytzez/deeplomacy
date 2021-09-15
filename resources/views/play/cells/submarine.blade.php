f5859408437af2832f3dba994ef75ac66dee5d4f
sytzez <sytzez@gmail.com>
2021-09-15 14:22:41 +0200

    [Feature] Very ugly form to attack or share sonar with submarines

--- resources/views/play/cells/submarine.blade.php
+++ resources/views/play/cells/submarine.blade.php
@@ -1,1 +1,20 @@
-🚢
+<a href="javascript:void(0)" onclick="this.parent.querySelector('options').hidden = ! this.parent.querySelector('options').hidden">
+    🚢
+</a>
+<div class="options">
+    <form method="post" action="{{ route('play.attack', [$game]) }}">
+        @csrf
+
+        <input type="hidden" name="submarine" value="{{ $cell->getSubmarine()->getModel()->id }}">
+        <input type="submit" value="Attack">
+
+    </form>
+
+    <form method="post" action="{{ route('play.share-sonar', [$game]) }}">
+        @csrf
+
+        <input type="hidden" name="submarine" value="{{ $cell->getSubmarine()->getModel()->id }}">
+        <input type="submit" value="Share sonar">
+
+    </form>
+</div>
