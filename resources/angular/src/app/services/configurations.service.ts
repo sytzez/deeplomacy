57dd26e5960765b74674aaa5e3b991690e74ac34
sytzez <sytzez@gmail.com>
2021-09-17 14:09:51 +0200

    [Feature] Create configurations service in frontend

---
+++ resources/angular/src/app/services/configurations.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from "./api.service";
+import { Observable } from "rxjs";
+import { Configuration } from "../models/configuration";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ConfigurationsService {
+
+    constructor(
+        protected api: ApiService,
+    ) {
+    }
+
+    public getAll(): Observable<Configuration[]> {
+        return this.api.get<Configuration[]>('configurations');
+    }
+
+    public getById(
+        id: string,
+    ): Observable<Configuration> {
+        return this.api.get<Configuration>(`configurations/${id}`);
+    }
+
+}
