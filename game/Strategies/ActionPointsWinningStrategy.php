97b6f3ef6828f561e0c5102f1631aae88e54d145
sytzez <sytzez@gmail.com>
2021-09-28 21:19:10 +0200

    [Feature] Reach a certain amount of action points winning strategy

---
+++ game/Strategies/ActionPointsWinningStrategy.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Game\Strategies;
+
+use Game\Contracts\GameContract;
+use Game\Contracts\SubmarineContract;
+use Game\Contracts\SubmarineRepositoryContract;
+use Game\Contracts\WinningStrategyContract;
+use Game\Data\ActionPoints;
+use Game\Data\VictoryData;
+
+class ActionPointsWinningStrategy implements WinningStrategyContract
+{
+    protected ActionPoints $goal = 100;
+
+    public function __construct(
+        protected SubmarineRepositoryContract $submarineRepository,
+    ) {
+    }
+
+    public function setGoal(ActionPoints $goal): static
+    {
+        $this->goal = $goal;
+
+        return $this;
+    }
+
+    function check(GameContract $game): VictoryData
+    {
+        $submarines = $this->submarineRepository->getAll($game);
+
+        $highestSubmarines = $this->getSubmarinesWithHighestActionPoints($submarines);
+
+        if (
+            empty($highestSubmarines)
+            || $highestSubmarines[0]->getActionPoints()->getAmount() < $this->goal->getAmount()
+        ) {
+            return new VictoryData(false);
+        }
+
+        return new VictoryData(true, $highestSubmarines);
+    }
+
+    /**
+     * @param iterable<SubmarineContract> $submarines
+     * @return iterable<SubmarineContract>
+     */
+    function getSubmarinesWithHighestActionPoints(iterable $submarines): iterable
+    {
+        $highestActionPoints = new ActionPoints(0);
+        $highestSubmarines = [];
+
+        foreach ($submarines as $submarine) {
+
+            if ($submarine->getActionPoints()->getAmount() > $highestActionPoints->getAmount()) {
+                $highestSubmarines = [$submarine];
+                $highestActionPoints = $submarine->getActionPoints();
+                continue;
+            }
+
+            if ($submarine->getActionPoints()->equals($highestActionPoints)) {
+                $highestSubmarines[] = $submarine;
+            }
+        }
+
+        return $highestSubmarines;
+    }
+}
